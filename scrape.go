81c0ac358681a1d68730761cfd2b3317201e5f64
anikamorris <anikamorris11@gmail.com>
2020-07-17 13:25:58 -0700

    returns array of apartments

--- scrape.go
+++ scrape.go
@@ -15,49 +15,15 @@ import (
    price per month, and number of bedrooms */
 type Apartment struct {
 	Location string
-	Sqft string
 	Price string
-	NumBedrooms string
+	Details string
+	URL string
 }
 
 /* struct to store the full listing before being processed */
 type FullListing struct {
 	Listing string
 }
-
-// func scrapeHandler() {
-// 	// Instantiate default collector
-// 	c := colly.NewCollector()
-
-// 	// On every a element which has href attribute call callback
-// 	c.OnHTML(".list-card-price", func(e *colly.HTMLElement) {
-// 		fmt.Printf("Price per month: %q\n", e.Text)
-// 	})
-
-// 	c.OnHTML(".list-card-details li:first-child", func(e *colly.HTMLElement) {
-// 		fmt.Printf("Number of bedrooms: %q\n", e.Text)
-// 	})
-
-// 	c.OnHTML(".list-card-addr", func(e *colly.HTMLElement) {
-// 		link := e.Attr("href")
-// 		fmt.Printf("Location: %q\nLink: %s\n", e.Text, link)
-// 	})
-
-// 	c.OnHTML(".list-card-details li:nth-child(3)", func(e *colly.HTMLElement) {
-// 		fmt.Printf("Sqft: %q\n", e.Text)
-// 	})
-
-// 	// Before making a request print "Visiting ..."
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("Visiting", r.URL.String())
-// 	})
-
-// 	// Start scraping on https://www.zillow.com/homes/for_rent/
-// 	c.Visit("https://www.zillow.com/homes/for_rent/")
-// }
-
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
 
 /* Writes listing to inputted file */
 func AppendListingToFile(filename string, e FullListing) {
@@ -79,10 +45,10 @@ func AppendListingToFile(filename string, e FullListing) {
     }
 }
 
-func homeView() {
+func homeView() []Apartment {
 	c := colly.NewCollector()
 	// Keep track of all listings that we've found so far
-	// var listings []FullListing
+	var listings []Apartment
 	// On every a element which has href attribute call callback
 	// c.OnHTML(".list-card-price", func(e *colly.HTMLElement) {
 	// 	fmt.Printf("Price per month: %q\n", e.Text)
@@ -90,7 +56,8 @@ func homeView() {
 	c.OnHTML(".list-card-link", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		fmt.Printf("Link found: %s -> %s \n", e.Text, link)
-		detailView(e.Text, link)
+		apartment := detailView(e.Text, link)
+		listings = append(listings, apartment)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -104,17 +71,21 @@ func homeView() {
 	})
 
 	c.Visit("https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.54679717016602%2C%22east%22%3A-122.31986082983398%2C%22south%22%3A37.68527794262908%2C%22north%22%3A37.8651955033787%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22filterState%22%3A%7B%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22isMapVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D")
-
+	return listings
 }
 
-func detailView(location string, link string) {
-	// var apartment Apartment
-	// apartment.Location = location
+func detailView(location string, link string) Apartment {
+	var apartment Apartment
+	apartment.Location = location
+	apartment.URL = link
 
 	c := colly.NewCollector()
 
-	c.OnHTML(".ds-bed-bath-living-area", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	c.OnHTML(".ds-summary-row", func(e *colly.HTMLElement) {
+		fmt.Println(e.ChildText(".ds-value"))
+		fmt.Println(e.ChildText(".ds-bed-bath-living-area"))
+		apartment.Details = e.ChildText(".ds-bed-bath-living-area")
+		apartment.Price = e.ChildText(".ds-value")
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -123,8 +94,9 @@ func detailView(location string, link string) {
 	})
 
 	c.Visit(link)
+	return apartment
 }
 
 func main() {
-	homeView()
+	fmt.Println(homeView())
 }
